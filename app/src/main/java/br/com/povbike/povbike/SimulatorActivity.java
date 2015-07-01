d17d10b6691d49ce803e17126c9c221024554851
Giovanni Marques <giovannimarques33@gmail.com>
2015-07-01 01:20:22 -0300

    Random generator

--- app/src/main/java/br/com/povbike/povbike/SimulatorActivity.java
+++ app/src/main/java/br/com/povbike/povbike/SimulatorActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +22,26 @@ public class SimulatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_simulator);
         mLedBoard = (LedBoard) findViewById(R.id.led_board);
 
+        mLedBoard.setBytes(generateBytes(16));
+        mLedBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLedBoard.setBytes(generateBytes(16));
+                mLedBoard.invalidate();
+            }
+        });
+    }
+
+    private List<Byte> generateBytes(int size) {
         Random rnd = new Random();
         rnd.setSeed(System.currentTimeMillis());
 
         List<Byte> bytes = new ArrayList<>();
-        for (int i = 0; i < 32; i++) {
-            bytes.add((byte) 0xff);
+        for (int i = 0; i < size; i++) {
+            bytes.add((byte) rnd.nextInt(255));
         }
 
-        mLedBoard.setBytes(bytes);
+        return bytes;
     }
+
 }
